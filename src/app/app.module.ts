febc98f5a4cceae9f94d90231ea68942afd252d4
Kevin Gómez <kevingomez.a98@gmail.com>
2020-05-04 14:25:42 +0200

    Creacion del componente clientes y del archivo clientes.json.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './components/share/header/header.component';
 import { FooterComponent } from './components/share/footer/footer.component';
 import { DirectivaComponent } from './components/pages/directiva/directiva.component';
 import {CommonModule} from '@angular/common';
+import { ClientesComponent } from './components/pages/clientes/clientes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    DirectivaComponent
+    DirectivaComponent,
+    ClientesComponent
   ],
   imports: [
     BrowserModule, CommonModule
---
+++ src/assets/data/clientes.json.ts
@@ -0,0 +1,7 @@
+import {Cliente} from '../../app/components/pages/clientes/cliente';
+
+export const CLIENTES: Cliente[] = [
+  {id: 1, name: 'Kevin', lastName: 'Gómez', email: 'kevingomez.a98@gmail.com', createAt: '2020-04-05'},
+  {id: 2, name: 'Kev', lastName: 'Góm', email: 'kevin@gmail.com', createAt: '2020-04-04'},
+  {id: 3, name: 'K', lastName: 'Gó', email: 'kevingomez@gmail.com', createAt: '2020-04-03'}
+];
